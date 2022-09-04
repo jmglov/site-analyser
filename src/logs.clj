21fdee7092a1904d6e63ae56b852874f11c7c8f1
Josh Glover <jmglov@gmail.com>
2022-09-04 11:53:00 +0200

    logs: add write-entries! and read-entries

--- src/logs.clj
+++ src/logs.clj
@@ -1,5 +1,6 @@
 (ns logs
   (:require [clojure.java.io :as io]
+            [babashka.fs :as fs]
             [com.grzm.awyeah.client.api :as aws]
             [parser]
             [util :refer [log error]])
@@ -90,56 +91,14 @@
          entries (->> logs
                       (mapcat (partial get-log-lines client))
                       (parser/parse-lines log-type))]
-     {:log-type log-type, :logs logs, :entries entries})))
+     {:date (str date), :log-type log-type, :logs logs, :entries entries})))
 
-(comment
+(defn write-entries! [dir {:keys [date] :as entries}]
+  (spit (fs/file dir (format "%s.edn" date))
+        (-> entries
+            (update :logs vec)
+            (update :entries vec))))
 
-  (def config {:aws-region "eu-west-1"
-               :s3-bucket "logs.jmglov.net"
-               :s3-prefix "logs/"
-               :cloudfront-dist-id "E1HJS54CQLFQU4"})
-  ;; => #<Var@8216104:
-  ;;      {:aws-region "eu-west-1",
-  ;;       :s3-bucket "logs.jmglov.net",
-  ;;       :s3-prefix "logs/",
-  ;;       :cloudfront-dist-id "E1HJS54CQLFQU4"}>
-
-  (def logs-client (mk-client config))
-
-  (list-logs logs-client "2022-09-03" :cloudfront {:limit 1})
-  ;; => ("logs/E1HJS54CQLFQU4.2022-09-03-00.18a84fe8.gz")
-
-  (list-logs logs-client "2022-09-03" :s3 {:limit 1})
-  ;; => ("logs/2022-09-03-00-11-39-EB3F7366DA9271E8")
-
-  (get-log-lines logs-client
-                 (first (list-cloudfront-logs logs-client
-                                              "2022-09-03"
-                                              {:limit 1})))
-  ;; => ("#Version: 1.0"
-  ;;     "#Fields: date time x-edge-location sc-bytes c-ip cs-method cs(Host) cs-uri-stem sc-status cs(Referer) cs(User-Agent) cs-uri-query cs(Cookie) x-edge-result-type x-edge-request-id x-host-header cs-protocol cs-bytes time-taken x-forwarded-for ssl-protocol ssl-cipher x-edge-response-result-type cs-protocol-version fle-status fle-encrypted-fields c-port time-to-first-byte x-edge-detailed-result-type sc-content-type sc-content-len sc-range-start sc-range-end"
-  ;;     "2022-09-03\t00:56:20\tDFW55-C1\t1919\t66.249.73.23\tGET\td3bulohh9org4y.cloudfront.net\t/\t200\t-\tMozilla/5.0%20(compatible;%20Googlebot/2.1;%20+http://www.google.com/bot.html)\t-\t-\tHit\toMQkwemTOCP7Y3Oj4ueAhdjPx1mhtotp20thm33N0BEkoNo8_xdSKg==\twww.jmglov.net\thttps\t293\t0.033\t-\tTLSv1.3\tTLS_AES_128_GCM_SHA256\tHit\tHTTP/1.1\t-\t-\t43350\t0.033\tHit\ttext/html\t-\t-\t-"
-  ;;     "2022-09-03\t00:56:20\tDFW55-C1\t580\t66.249.73.28\tGET\td3bulohh9org4y.cloudfront.net\t/\t301\t-\tMozilla/5.0%20(compatible;%20Googlebot/2.1;%20+http://www.google.com/bot.html)\t-\t-\tRedirect\tCPL5Efkr9IFJcSzwkioyS8_wl_OlMatuxKWl7rackaFjdYXi2JZs2w==\twww.jmglov.net\thttp\t293\t0.000\t-\t-\t-\tRedirect\tHTTP/1.1\t-\t-\t47050\t0.000\tRedirect\ttext/html\t183\t-\t-")
-
-  (get-log-lines logs-client
-                 (first (list-s3-logs logs-client
-                                      "2022-09-03"
-                                      {:limit 1})))
-  ;; => ("022d83ad6361dec3c93757e75c1c3a7982532ffdbf3bf87976490873591e2188 jmglov.net [02/Sep/2022:23:11:28 +0000] 64.252.70.219 - 8K9166G4KGR6EACQ WEBSITE.GET.OBJECT blog/assets/2022-09-02-code1.png \"GET /blog/assets/2022-09-02-code1.png HTTP/1.1\" 304 - - 151328 21 - \"-\" \"Amazon CloudFront\" - q5O1Lp5Toi1BJoene+95P+3xwSGVgqpyb39MP9rLh+ll0v1RoH+HuliLcAV7LmVCBLd73XYPa3g= - - - jmglov.net.s3-website-eu-west-1.amazonaws.com - -")
-
-  (get-log-entries logs-client "2022-09-03" :cloudfront {:limit 2})
-
-  (get-log-entries logs-client "2022-09-03" :s3 {:limit 2})
-
-  (let [date "2022-09-03"]
-    (spit (format "/tmp/cf-logs.%s.edn" date)
-          (-> (get-log-entries logs-client date :cloudfront {:limit 2})
-              (update :logs vec)
-              (update :entries vec))))
-  ;; => nil
-
-  (let [date "2022-09-03"]
-    (-> (slurp (format "/tmp/cf-logs.%s.edn" date))
-        read-string))
-
-  )
+(defn read-entries [dir date]
+  (with-open [r (io/reader (fs/file dir (format "%s.edn" date)))]
+    (read (java.io.PushbackReader. r))))
