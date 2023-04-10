4cd983c54445ecd652a85ed03788b6c9317a3859
Josh Glover <jmglov@gmail.com>
2023-04-10 16:48:14 +0200

    Upgrade babashka in shell.nix

--- shell.nix
+++ shell.nix
@@ -13,21 +13,15 @@ let
     {
       linux = {
         aarch64 =
-          "7f90e48832d498b77c260dd5af03c4206b2bb175726d21616737537e75b0d141";
+          "f187204e38f2ae430fb0f167807022d43c32e15e8f91c225819e8a72965092f6";
         amd64 =
-          "c72d1682d2a77421939f63987ceeca09bbd8086f96de5e1ed1a46a9a70eada88";
-      };
-      macos = {
-        aarch64 =
-          "11c4b4bd0b534db1ecd732b03bc376f8b21bbda0d88cacb4bbe15b8469029123";
-        amd64 =
-          "379a4f50ba302d3c5a5e2687f9e1dfe02acbd9a87e0b4619f4f0791326f340d0";
+          "b1772d2b04399ed981803b380307888a58068803ecf2c0cd22c43e83b811a9f8";
       };
     }.${osName}.${arch};
 
   babashka = stdenv.mkDerivation rec {
     pname = "babashka";
-    version = "1.0.168";
+    version = "1.3.176";
 
     src = builtins.fetchurl {
       inherit sha256;
